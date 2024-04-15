e2c398c59f8a2c0be10940a6200fa5554086bfd6
Albertocaen <albertojesusbrito1999@gmail.com>
2024-04-15 03:27:24 +0200

    First Commit v1

---
+++ src/main/java/org/proyecto/fastdeliveryp_v1/FastDeliveryPV1Application.java
@@ -0,0 +1,13 @@
+package org.proyecto.fastdeliveryp_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDeliveryPV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastDeliveryPV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/proyecto/fastdeliveryp_v1/FastDeliveryPV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.proyecto.fastdeliveryp_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastDeliveryPV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e273d97ee6e308a6fc53721fe0527e4faeaf75ee
Albertocaen <albertojesusbrito1999@gmail.com>
2024-06-05 01:39:03 +0200

    terminado de front basico, proximo paso carrito y limpiar codigo

--- src/main/java/org/proyecto/fastdeliveryp_v1/repository/ProductoRepository.java
+++ src/main/java/org/proyecto/fastdeliveryp_v1/repository/ProductoRepository.java
@@ -4,10 +4,13 @@ import org.proyecto.fastdeliveryp_v1.entity.Producto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
     @Query("SELECT p FROM Producto p ORDER BY p.precio DESC")
     List<Producto> findTopProductos(Pageable pageable);
+
+    Page<Producto> findAll(Pageable pageable);
 }
